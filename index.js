6beb17c03dfa154e341b4619d46d9a27d34e03a6
Cristian Pinto <cristian.pinto@sage.com>
2017-10-15 03:01:22 +0200

    wip

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const builder = require("botbuilder");
 const request = require("request-promise");
-// const locationDialog = require('botbuilder-location');
-const appUrl = process.env.APP_URL;
-const apiUrl = "";
+const locationDialog = require('botbuilder-location');
+const continents = require("./places.json").Continents;
+const APIUrl = "https://flightadvisorbackend.herokuapp.com";
+const FB_API_KEY = process.env.FB_API_KEY;
+const FB_URL = "https://graph.facebook.com/v2.6/";
+const _ = require("lodash");
 
 //create an express server
 var app = express();
@@ -31,34 +34,76 @@ var bot = new builder.UniversalBot(connector, {
 //hook up bot endpoint
 app.post("/api/messages", connector.listen());
 
+let optionsWelcomeIntent = {
+  recognizers: [new builder.LuisRecognizer({
+    "en": process.env.LUIS_URL_EN_WELCOME
+  })]
+}
 
+bot.library(locationDialog.createLibrary(process.env.BING_MAPS_API_KEY));
 
-bot.dialog("/", [
-	(session, results, next) => {
+bot.dialog('/', [
+  (session, results, next) => {
+    let name = session.message.address.user.name.split(" ")[0];
+    session.send("welcome-message", name);
 
-		let test = session.gettext("what-do-you-want-to-do");
-		let msg = new builder.Message(session)
-			.text(test)
-			.attachments([new builder.Keyboard(session)
-				.buttons([
-					builder.CardAction.imBack(session, session.gettext("manage-saving-fund"), "manage-saving-fund"),
-					builder.CardAction.imBack(session, session.gettext("flight-planning"), "flight-planning")
-				])
-			]);
-
-		builder.Prompts.text(session, msg);
-	},
-	(session, results, next) => {
-
-		let message = session.message.text.toLowerCase();
-		if(message.includes("fund")) {
-			session.beginDialog("saving-fund");
-		} else if(message.includes("flight")) {
-			session.beginDialog("flight-planning");
-		}
-
+    let options = {
+			prompt: "Tell us where it's going to be your travel origin normally.",
+			repropmt: "I can't find that location, try it again.",
+      useNativeControl: true,
+      reverseGeocode: true,
+			skipFavorites: true,
+			skipConfirmationAsk: true
+    };
+		locationDialog.getLocation(session, options);
+  },
+  (session, results, next) => {
+    session.send("Let's continue!");
+    session.userData.origin = results.response.locality ? results.response.locality : results.response.region;
+    // TODO: Request to know nearest shops
+    session.userData.origin = searchCodeLocation(session.userData.origin);
+		session.beginDialog("welcome");
 	}
 ])
+
+let rootDialog = new builder.IntentDialog(optionsWelcomeIntent)
+  .onBegin((session, results) => {
+
+    let test = session.gettext("what-do-you-want-to-do");
+    let msg = new builder.Message(session)
+      .text(test)
+      .attachments([new builder.Keyboard(session)
+        .buttons([
+          builder.CardAction.imBack(session, session.gettext("manage-saving-fund"), "manage-saving-fund"),
+          builder.CardAction.imBack(session, session.gettext("flight-planning"), "flight-planning")
+        ])
+      ]);
+
+    session.send(msg);
+  })
+  .onDefault((session, results, next) => {
+    session.send("I'm so sorry but I don't understand that.")
+  })
+  .matches("manageMoney", "saving-fund")
+  .matches("planFlight", "flight-planning")
+
+bot.dialog("welcome", rootDialog);
+
+// bot.dialog("/", [
+// 	(session, results, next) => {
+    
+// 	},
+// 	(session, results, next) => {
+
+// 		let message = session.message.text.toLowerCase();
+// 		if(message.includes("fund")) {
+// 			session.beginDialog("saving-fund");
+// 		} else if(message.includes("flight")) {
+// 			session.beginDialog("flight-planning");
+// 		}
+
+// 	}
+// ])
 
 bot.dialog("saving-fund", [
 	(session, results, next) => {
@@ -83,6 +128,13 @@ bot.dialog("saving-fund", [
 
 bot.dialog("flight-planning", [
 	(session, results, next) => {
+    if(results.entities.length > 0){
+      for(let entity of results.entities) {
+        if(entity.type == "builtin.geography.country") {
+          session.userData.destination = searchCodeLocation(entity.entity[0].toUpperCase() + entity.entity.slice(1));
+        }
+      }
+    }
 		let title = session.gettext("choose-an-option");
 		let msg = new builder.Message(session)
 			.text(title)
@@ -104,8 +156,166 @@ bot.dialog("flight-planning", [
 	}
 ])
 
+
 bot.dialog("where-can-i-go", [
 	(session, results, next) => {
+		session.beginDialog("when-and-where");
+	},
+	(session, results, next) => {
+    
+    session.dialogData.from = results.where;
+    session.dialogData.when = results.when;
+
+    // TODO: query to SkyScanner Api for country recommendations
+
+    request({
+      url: APIUrl + "/countries",
+      qs: {
+        origin: "ES",
+        locale: session.preferredLocale(),
+        availability: session.dialogData.when
+      },
+      json: true
+    })
+      .then((recomendations) => {
+    
+        let elements = [{
+          title: "TOP 3 cheapest countries to visit",
+          subtitle: "Press on 'Check more expensive countries' if any of the list suits your preferences",
+          image_url: "https://www.elnacional.cat/uploads/s1/23/28/12/6/DHiit56XoAI2IKB_1_630x630.jpg"
+        }];
+    
+    
+        for(let recomendation of recomendations.slice(0, 3)) {
+          let elem = {
+            // picture, title(country name), price from 
+            title: recomendation.destination,
+            subtitle: session.gettext("price-from", recomendation.price),
+            image_url: recomendation.imgUrl,
+            buttons: [
+              {
+                type: "postback",
+                title: session.gettext("view-cities"),
+                payload: recomendation.countryId ? recomendation.countryId : recomendation.destination
+              }
+            ]
+          }
+          elements.push(elem);
+        }
+        
+        let msg = new builder.Message(session).sourceEvent({
+          //specify the channel
+          facebook: {
+            //format according to channel's requirements
+            //(in our case, the above JSON required by Facebook)
+            attachment: {
+              type: "template",
+              payload: {
+                "template_type": "list",
+                "top_element_style": "large",
+                elements,
+                "buttons": [
+                  {
+                    "title": "Check more expensive countries",
+                    "type": "postback",
+                    "payload": "payload"            
+                  }
+                ]  
+              }
+            } //end of attachment
+          }
+        })
+        
+        builder.Prompts.text(session, msg);
+        
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    
+		
+  },
+  (session, results, next) => {
+    let placeId = results.response;
+    // TODO: http get request to skyscanner api to get flights to placeId country
+
+    let flights = [
+      {
+        price: 123,
+        origin: "Barcelona",
+        destination: "London Gatwick",
+        returnDate: "13/11/2017",
+        outboundDate: "10/11/2017",
+        flightNum: "VY4343",
+        imageUrl: "https://www.elnacional.cat/uploads/s1/23/28/12/6/DHiit56XoAI2IKB_1_630x630.jpg"
+      },
+      {
+        price: 321,
+        origin: "Barcelona",
+        destination: "Manchester",
+        returnDate: "17/11/2017",
+        outboundDate: "14/11/2017",
+        flightNum: "VY4343",
+        imageUrl: "https://www.elnacional.cat/uploads/s1/23/28/12/6/DHiit56XoAI2IKB_1_630x630.jpg"
+      }
+    ]
+
+    let elements = [{
+      title: "TOP 3 cheapest cities to go of United Kingdom",
+      subtitle: "Press on 'Check more expensive cities' if any of the list suits your preferences" ,
+      image_url: "https://www.elnacional.cat/uploads/s1/23/28/12/6/DHiit56XoAI2IKB_1_630x630.jpg"
+    }];
+    
+    for(let flight of flights.slice(0, 3)) {
+      let elem = {
+        // picture, title(country name), price from 
+        title: `From ${flight.origin} to ${flight.destination} for ${flight.price}€`,
+        subtitle: `Outbound: ${flight.outboundDate}\nReturn: ${flight.returnDate}`,
+        image_url: flight.imageUrl,
+        buttons: [
+          {
+            type: "postback",
+            title: "View more flights",
+            payload: flight.flightNum
+          }
+        ]
+      }
+      elements.push(elem);
+    }
+    
+    let msg = new builder.Message(session).sourceEvent({
+      //specify the channel
+      facebook: {
+        //format according to channel's requirements
+        //(in our case, the above JSON required by Facebook)
+        attachment: {
+          type: "template",
+          payload: {
+            "template_type": "list",
+            "top_element_style": "compact",
+            elements,
+            "buttons": [
+              {
+                "title": "Check more expensive cities",
+                "type": "postback",
+                "payload": "payload"            
+              }
+            ]  
+          }
+        } //end of attachment
+      }
+    })
+        
+    builder.Prompts.text(session, msg);
+    
+  },
+  (session, results, next) => {
+    console.log(results);
+  }
+])
+
+bot.dialog("when-and-where", [
+  (session, results, next) => {
 		let title = session.gettext("when-want-to-go");
 		
 		let msg = new builder.Message(session)
@@ -122,6 +332,11 @@ bot.dialog("where-can-i-go", [
 	},
 	(session, results, next) => {
 
+    // TODO: check that it's a correct answer
+    let when = session.message.text.toLowerCase();
+
+    session.dialogData.when = when;
+
 		let title = session.gettext("which-departure-airport");
 
 		let cityFB = "Barcelona";
@@ -137,74 +352,13 @@ bot.dialog("where-can-i-go", [
 			]);
 
 		builder.Prompts.text(session, msg);
-	},
-	(session, results, next) => {
-		let message = session.message.text.toLowerCase();
-		let recomendations = [
-			{
-        countryName: "United Kingdom",
-        countryId: "abc",
-				priceFrom: 123,
-				imageUrl: "https://www.elnacional.cat/uploads/s1/23/28/12/6/DHiit56XoAI2IKB_1_630x630.jpg"
-			},
-			{
-        countryName: "Germany",
-        countryId: "cba",
-				priceFrom: 321,
-				imageUrl: "https://www.elnacional.cat/uploads/s1/23/28/12/6/DHiit56XoAI2IKB_1_630x630.jpg"
-			}
-		]
-
-    let elements = [];
-
-		for(let recomendation of recomendations.slice(0, 4)) {
-			let elem = {
-        // picture, title(country name), price from 
-        title: recomendation.countryName,
-        subtitle: session.gettext("price-from", recomendation.priceFrom),
-        image_url: recomendation.imageUrl,
-        buttons: [
-          {
-            type: "postback",
-            title: session.gettext("view-cities"),
-            payload: recomendation.countryId
-          }
-        ]
-      }
-
-      elements.push(elem);
-		}
-    
-    let msg = new builder.Message(session).sourceEvent({
-      //specify the channel
-      facebook: {
-        //format according to channel's requirements
-        //(in our case, the above JSON required by Facebook)
-        attachment: {
-          type: "template",
-          payload: {
-            "template_type": "list",
-            "top_element_style": "compact",
-            elements,
-            "buttons": [
-              {
-                "title": "View More",
-                "type": "postback",
-                "payload": "payload"            
-              }
-            ]  
-          }
-        } //end of attachment
-      }
-    })
-		
-		// builder.Prompts.text(msg);
-		session.send(msg);
-    
-	}
+  },
+  (session, results, next) => {
+    let where = session.message.text.toLowerCase();
+    session.endDialogWithResult({ where,
+                                  when: session.dialogData.when });
+  }
 ])
-
-
 
 
 bot.use({
@@ -223,3 +377,18 @@ bot.use({
 		}
 	]
 })
+
+
+
+
+function searchCodeLocation(locationName) {
+  for(let continent of continents) {
+    for(let country of continent.Countries) {
+      if(country.Name === locationName) return country.Id;
+      for(let city of country.Cities) {
+        if(city.Name === locationName) return city.IataCode;
+      }
+    }
+  }
+  return null;
+}
